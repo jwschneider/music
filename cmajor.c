7aff9e53ff60c9c261eff45d3c5249dacb15509e
John Schneider <johnschneider@berkeley.edu>
2017-06-13 17:00:17 -0700

    Added implementation of piano notes in haskell

--- cmajor.c
+++ cmajor.c
@@ -6,9 +6,21 @@
 #define SF 44100
 #define PI 3.1415926
 
-int main() {
-	double note = 261.1;
-		for(double t = 0; t < SF; t++) {
-			fputc((int) MAX*sin(2 * PI * t / note), stdout);
-		}
+double freq(int n) {
+	double power = (n - 49) / 12;
+	double base = pow(2, power);
+	return base * 440;
 }
+
+int main() {
+	double note = freq(71);
+	while(1) {
+		for(double t = 0; t < SF; t++) {
+			double v = 2 * PI * t / note;
+			fputc((int) MAX*sin(v), stdout);
+		}
+	}
+	return 0;
+}
+
+
---
+++ haskell/haskell-tut.hs
@@ -0,0 +1,17 @@
+import Data.List
+import System.IO
+
+samplingRate = 44100 :: Double
+bitDepth = 65536 :: Double
+
+{- Takes in the number of the key on the piano and returns the corresponding
+	frequency in hz -}
+keyFreq :: Int -> Double
+keyFreq n 
+	| (n >= 1) && (n <= 88) = (2 ** ((fromIntegral (n - 49)) / 12)) * 440
+	| otherwise = 0
+{- Takes in a Key number and a Time in seconds, and returns 
+	the list of doubles to be interpereted by pacat at 44.1k s16le -}
+playNote :: Int -> Double -> [Double]
+playNote key time = map (\ x -> bitDepth*sin(2*pi*x/(keyFreq key))) [0..((samplingRate*time)-1)]
+
