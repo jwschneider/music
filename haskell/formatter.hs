7c845409f3ca81cfdb80ced48645dc7bda8a1fc1
John Schneider <johnschneider@berkeley.edu>
2017-07-10 23:02:58 -0700

    Correctly implemented divDif and stripDif, with the exception that vals needs to be passed in as an argument

--- haskell/formatter.hs
+++ haskell/formatter.hs
@@ -2,6 +2,8 @@ import Data.List
 import System.IO
 import System.Environment
 import System.Random
+import Data.Matrix hiding (toList)
+import Data.Vector hiding (tail, length, map)
 
 {- author: John Schneider -}
 
@@ -39,6 +41,22 @@ getDoubles = do
 		first <- getLine
 		rest <- getDoubles
 		return (read first : rest)
+
+{- Returns the table of divided difference of the matrix mat starting with entry (i,j).
+	all of the entries are tuples, with (val, ?modified) -}
+divDif :: Matrix (Double, Double) -> (Int, Int) -> Matrix (Double, Double)
+divDif mat (i, j)
+	|((snd (getElem i j mat)) == 1) = mat
+	|(j==1) = setElem ((vals!!(i-1)), 1) (i, j) mat
+	|otherwise = do
+	let mat1 = divDif mat (i-1, j-1)
+	let mat2 = divDif mat1 (i, j-1)
+	setElem ((((fst (getElem i (j-1) mat2)) - (fst (getElem (i-1) (j-1) mat2)))/(fromIntegral (j-1))), 1) (i, j) mat2
+
+{- Strips the matrix of divided differences down to a list of elements -}
+stripDif :: Matrix (Double, Double) -> [Double]
+stripDif mat = map (\(i, j) -> i) (toList (getDiag mat))
+
 
 main = do
 	time <- getLine
--- haskell/test-matrices.hs
+++ haskell/test-matrices.hs
@@ -1,13 +1,20 @@
-import Data.Matrix
+import Data.Matrix hiding (toList)
+import Data.Vector hiding (length, map)
 
-vals = [3, 5, 7] :: [Double]
+vals = [3, 5, 7, 11, 13, 17, 19] :: [Double]
 
 divDif :: Matrix (Double, Double) -> (Int, Int) -> Matrix (Double, Double)
 divDif mat (i, j)
 	|((snd (getElem i j mat)) == 1) = mat
 	|(j==1) = setElem ((vals!!(i-1)), 1) (i, j) mat
-	|otherwise = setElem ((((fst(getElem i (j-1) (divDif mat (i, j-1)))) - (fst(getElem (i-1) (j-1) (divDif mat (i-1, j-1)))))/(fromIntegral (j-1))), 1) (i, j) (divDif mat (i, j-1))
+	|otherwise = do
+	let mat1 = divDif mat (i-1, j-1)
+	let mat2 = divDif mat1 (i, j-1)
+	setElem ((((fst (getElem i (j-1) mat2)) - (fst (getElem (i-1) (j-1) mat2)))/(fromIntegral (j-1))), 1) (i, j) mat2
 
-mat = matrix 3 3 (\(i, j) -> (0::Double, 0::Double))
+stripDif :: Matrix (Double, Double) -> [Double]
+stripDif mat = map (\(i, j) -> i) (toList (getDiag mat))
 
-main = print $ divDif mat (3, 3)
+mat = matrix (length vals) (length vals) (\(i, j) -> (0::Double, 0::Double))
+
+main = print $ stripDif (divDif mat ((length vals), (length vals)))
